baeaa87e6847b1098db4979614949dfd11a5821f
Chamoli <godchamoli@gmail.com>
2024-09-17 20:22:46 +0530

    chore: Update constants.js with environment variables for API URLs

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,8 @@
+// constants.js
 export const constants = {
-    "REGISTER_USER_URL": "http://localhost:6969/api/auth/user/v1/register",
-    "LOGIN_USER_URL": "http://localhost:6969/api/auth/user/v1/login",
-    "BASE_URL_USER": "http://localhost:6969/api/auth/user/v1",
-    "BASE_URL_FLASHCARD": "http://localhost:6969/api/auth/flashcard/v1",
-}
+    REGISTER_USER_URL: `${import.meta.env.VITE_BASE_URL}/api/auth/user/v1/register`,
+    LOGIN_USER_URL: `${import.meta.env.VITE_BASE_URL}/api/auth/user/v1/login`,
+    BASE_URL_USER: `${import.meta.env.VITE_BASE_URL}/api/auth/user/v1`,
+    BASE_URL_FLASHCARD: `${import.meta.env.VITE_BASE_URL}/api/auth/flashcard/v1`,
+  };
+  
